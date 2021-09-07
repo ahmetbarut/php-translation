de4a72e3f506973680862e3d2b82e300c74788f3
Ahmet Barut <ahmetbarut588@gmail.com>
2021-09-08 01:10:50 +0300

    First

---
+++ src/Loader/ArrayLoader.php
@@ -0,0 +1,57 @@
+<?php
+
+namespace ahmetbarut\Translation\Loader;
+
+
+class ArrayLoader implements ILoader
+{
+    /**
+     * Returns the specified page in an array.
+     *
+     * @var object
+     */
+    public $translations;
+
+    /**
+     * directory of files
+     *
+     * @var string
+     */
+    public $path;
+
+    /**
+     * Returns the content of the requested file.
+     *
+     * @param string $file
+     * @return void
+     */
+    public function resolveFile($path, $locale)
+    {
+        $this->path = $path . '/' . $locale;
+        return $this;
+    }
+
+    /**
+     * Returns the value of the relevant key.
+     *
+     * @param string $key
+     * @return void
+     */
+    public function getFile($file)
+    {
+        $this->translations = include $this->path . '/' . $file . '.php';
+    }
+
+    /**
+     * Resolves the directory where the files are located
+     * @param string $path
+     * @param string $locale
+     * @return void
+     */
+    public function key($key)
+    {
+        $key = explode('.', $key);
+        $this->getFile($key[0]);
+        return $this->translations[$key[1]];
+    }
+}
---
+++ src/Loader/JsonLoader.php
@@ -0,0 +1,58 @@
+<?php
+
+namespace ahmetbarut\Translation\Loader;
+
+use ahmetbarut\Translation\Loader\ILoader;
+
+class JsonLoader implements ILoader
+{
+    /**
+     * Returns the specified page in an array.
+     *
+     * @var object
+     */
+    public $translations;
+
+    /**
+     * directory of files
+     *
+     * @var string
+     */
+    public $path;
+
+    /**
+     * Returns the content of the requested file.
+     *
+     * @param string $file
+     * @return void
+     */
+    public function getFile($file)
+    {
+        $this->translations = json_decode(file_get_contents($this->path . '/' . $file . '.json'), null);
+    }
+
+    /**
+     * Returns the value of the relevant key.
+     *
+     * @param string $key
+     * @return void
+     */
+    public function key($key)
+    {
+        $key = explode('.', $key);
+        $this->getFile($key[0]);
+        return $this->translations->{$key[1]};
+    }
+
+    /**
+     * Resolves the directory where the files are located
+     * @param string $path
+     * @param string $locale
+     * @return void
+     */
+    public function resolveFile($path, $locale)
+    {
+        $this->path = $path . '/' . $locale;
+        return $this;
+    }
+}
